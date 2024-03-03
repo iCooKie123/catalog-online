83415b4b483a675a72710fb20ccd4ee86122bf99
Alex <aconstantin86@yahoo.com>
2024-03-04 01:29:54 +0200

    Add profile page export

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,3 +1,4 @@
 export * from "./home";
 export * from "./test-page";
 export * from "./login-page";
+export * from "./profile-page";
---
+++ src/pages/profile-page/ProfilePage.tsx
@@ -0,0 +1,8 @@
+export const ProfilePage = () => {
+	return (
+		<div>
+			<h1>Profile Page</h1>
+			<p>Welcome to the profile page!</p>
+		</div>
+	);
+};
