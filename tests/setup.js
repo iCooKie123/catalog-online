105f138202890e1cb8d3ef39c5ed045ade297171
Alex <aconstantin86@yahoo.com>
2024-03-23 19:22:14 +0200

    Remove axios mock and reset mockAxios in tests setup from deleted library

--- tests/setup.js
+++ tests/setup.js
@@ -1,8 +1,7 @@
 import { afterEach } from "vitest";
 import { cleanup } from "@testing-library/react";
 import "@testing-library/jest-dom/vitest";
-import mockAxios from "vitest-mock-axios";
+
 afterEach(() => {
 	cleanup();
-	mockAxios.reset();
 });
